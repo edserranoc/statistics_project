8d632c012cd794031c076eef9351e88206b93422
edserranoc <edserranoc@unal.edu.co>
2024-05-17 10:39:29 -0600

    Upload statistics_methods.py

---
+++ statistics_methods.py
@@ -0,0 +1,13 @@
+import numpy as np
+from typing import Tuple, Callable
+
+class stats_models:
+    @staticmethod
+    def logLikelihood_NormalCEN():
+        pass
+    
+
+class stats_distributions:
+    @staticmethod
+    def logLikelihood_NormalCEN():
+        pass
