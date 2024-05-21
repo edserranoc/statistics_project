30cfe7338769be4a4db4c9409fc1f3e3819a2097
Ezau Faridh Torres Torres <ezau.torres@cimat.mx>
2024-05-20 20:12:47 -0600

    Upgrade stats_mod

--- stats_mod.py
+++ stats_mod.py
@@ -60,7 +60,7 @@ class stats_models:
         mu = vec[1]
         beta = mu / alpha
         if alpha > 0 and mu > 0:
-            dif = stats.gamma.cdf(DATMAT[:, 1], a=alpha, scale=beta) - stats.gamma.cdf(DATMAT[:, 0], a=alpha, scale=beta)
+            dif = stats.gamma.cdf(DATMAT[:, 1], shape=alpha, scale=beta) - stats.gamma.cdf(DATMAT[:, 0], shape=alpha, scale=beta)
             dif[dif < eps] = eps
             lv = np.sum(np.log(dif))
         else:
@@ -101,7 +101,7 @@ class stats_models:
         eps = 1e-6
         theta = vec
         if theta > 0:
-            dif = stats.expon.cdf(DATMAT[:, 1], scale=theta) - stats.expon.cdf(DATMAT[:, 0], scale=theta)
+            dif = stats.expon.cdf(DATMAT[:, 1], rate=1/theta) - stats.expon.cdf(DATMAT[:, 0], rate=1/theta)
             dif[dif < eps] = eps
             lv = np.sum(np.log(dif))
         else:
@@ -122,7 +122,7 @@ class stats_models:
         beta = vec[0]
         sigma = vec[1]
         if beta > 0 and sigma > 0:
-            dif = stats.weibull_min.cdf(DATMAT[:, 1], c=sigma, scale=beta) - stats.weibull_min.cdf(DATMAT[:, 0], c=sigma, scale=beta)
+            dif = stats.weibull_min.cdf(DATMAT[:, 1], shape = beta, scale=sigma) - stats.weibull_min.cdf(DATMAT[:, 0], shape = beta, scale=sigma)
             dif[dif < eps] = eps
             lv = np.sum(np.log(dif))
         else:
@@ -143,9 +143,31 @@ class stats_models:
         a = vec[0]
         b = vec[1]
         if b > 0:
-            dif = stats.gumbel_r.cdf(DATMAT[:, 1], loc=a, scale=b) - stats.gumbel_r.cdf(DATMAT[:, 0], loc=a, scale=b)
+            dif = stats.gumbel_r.cdf(DATMAT[:, 1], shape=a, scale=b) - stats.gumbel_r.cdf(DATMAT[:, 0], shape=a, scale=b)
             dif[dif < eps] = eps
             lv = np.sum(np.log(dif))
         else:
             lv = -np.inf
         return lv
+    
+    @staticmethod
+    def Weibetmle2(BETA: float, XX: np.array, NN: float, TT: float) -> float:
+        """
+        Compute the log-likelihood of the Censored Weibull model.
+        :param BETA: the shape parameter of the Weibull distribution.
+        :rtype BETA: float.
+        :param XX: the data matrix.
+        :rtype XX: np.array.
+        :param NN: the number of observations.
+        :rtype NN: float.
+        :param TT: the censoring threshold.
+        :rtype TT: float.
+        :return: the log-likelihood of the model.
+        """
+        if BETA > 0:
+            k1 = np.sum(np.exp(BETA * XX))
+            k2 = np.sum(XX * np.exp(BETA * XX))
+            sali = (NN / BETA) + TT - (NN * k2 / k1)
+        else:
+            sali = 5555
+        return sali
